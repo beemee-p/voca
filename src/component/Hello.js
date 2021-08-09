0a104374db545a5f304a177550d18adfc30d3700
beemee-p <raina.y.park@gmail.com>
2021-08-09 23:55:27 +0900

    Practice

---
+++ src/component/Hello.js
@@ -0,0 +1,12 @@
+import World from './World';
+import styles from "./Hello.module.css";
+
+export default function Hello() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <div className={styles.box}>Hello</div>
+      <World />
+    </div>
+  );
+}
