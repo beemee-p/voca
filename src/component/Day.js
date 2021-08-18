1fbdc18208ab9f4f0e715f942b32c806af748613
beemee-p <raina.y.park@gmail.com>
2021-08-19 00:15:46 +0900

    Replace dummy json to api

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,23 +1,27 @@
-import { useParams} from "react-router-dom"
-import dummy from "../db/data.json";
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
 import Word from "./Word"
 
 export default function Day() {
-  // const day = useParams().day
   const { day } = useParams()
-  console.log({day})
-  const wordList = dummy.words.filter(word => (
-    word.day === Number(day)
-  ))
-  console.log(wordList);
+  const [words, setWords] = useState([])
 
+  useEffect(() => {
+    fetch(`http://localhost:3001/words?day=${day}`) 
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setWords(data)
+      })
+  }, [day])
   
   return (
     <div>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map((word) => (
+          {words.map((word) => (
             <Word word={word} key={word.id}/>
           ))}
         </tbody>
